aa6968f268046b096485a7bcc659b8be07ba74ed
Pritam Maity <pritammaity071@gmail.com>
2026-08-30 03:16:50 +0530

    Connect frontend to production backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 import axios from 'axios'
 
 const api = axios.create({
+    // baseURL: import.meta.env.DEV
+    //     ? 'http://localhost:3000'
+    //     : '/api',
+
     baseURL: import.meta.env.DEV
         ? 'http://localhost:3000'
-        : '/api',
+        : 'https://day02-leetcode-auth-backend.onrender.com',
 
     withCredentials: true,
 })
